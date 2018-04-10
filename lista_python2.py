b20caa97e04ed252248ce18230d23b4e8b68190b
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2018-04-09 22:03:08 -0300

    Alterações pontuais

--- lista_python2.py
+++ lista_python2.py
@@ -2,7 +2,7 @@
 
 #exercicio 1
 
-#num = int(input("Digite um numero: "))
+num = int(input("Digite um numero: "))
 if (num == 0):
     print("O numero e zero")
 elif (num > 0):
@@ -42,41 +42,44 @@ print("Sua mae o teve com ",nasc,"anos de idade")
 print("-"*50)
 
 #exercicio 7
-um = int(input("Digite um numero: "))
-f(num % 2 == 0):
+num = int(input("Digite um numero: "))
+if(num % 2 == 0):
     print("O numero e par")
 else:    print("O numero e impar")
 
 #exercicio 8
 num1 = int(input("Primeiro numero: "))
 num2 = int(input("Segundo numero: "))
-f(num1>num2):
+if(num1>num2):
    print("Imprimindo o maior valor")
-    print(num1)
-    print("Imrimindo o menor valor")
-    print(num2)
+   print(num1)
+   print("Imrimindo o menor valor")
+   print(num2)
 else:
-    print("Imprimindo o maior valor")
-   print(num2#)
+   print("Imprimindo o maior valor")
+   print(num2)
    print("Imprimindo o menor valor")
    print(num1)
-exercicio 9
-um = 3
-f(num.__class_ is int):
+
+#exercicio 9
+num = 3
+if(num.__class_ is int):
    print("O numero ",num,"e do tipo inteiro")
-lse:
+else:
    print("O numero ", num, "nao e do tipo inteiro")
 
-#exercicio 10xt = "Olá"
-f(txt.__class__ is str):
+#exercicio 10
+txt = "Olá"
+if(txt.__class__ is str):
    print("O texto",txt,"e do tipo string")
-lse:
+else:
    print("O texto", txt, "nao e do tipo string#")
 
-#exercicio 11num = 5.4#
+#exercicio 11
+num = 5.4
 if(num.__class__ is float):
     print("O valor",num,"e do tipo decimal")
 else:
     print("O valor", num, "nao e do tipo decimal")
 
-#exercicio 12
+#exercicio 12   
