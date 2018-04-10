35d85e0e9cdd8a0b5e2115585708e8ea2e8f2036
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2018-04-09 21:56:06 -0300

    Lista Python 2

--- lista_python.py
+++ lista_python.py
@@ -1,90 +1,92 @@
-#exercicio 1
+# -*- coding: utf-8 -*- #
+
+# exercicio 1
 print("Leonardo Oliveira Novais da Silva")
 
-#exercicio 2
+# exercicio 2
 nome = input("Digite seu nome: ")
 print("O seu nome é", nome)
 
-#exercicio 3
+# exercicio 3
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 print("O seu nome é", nome, "e a sua idade é", idade)
 
-#exercicio 4
+# exercicio 4
 num = int(input("Digite um número: "))
 print(num)
 
-#exercicio 5
+# exercicio 5
 num = int(input("Digite um número: "))
-print("O número digitado foi",num)
+print("O número digitado foi", num)
 
-#exercicio 6
+# exercicio 6
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 num3 = int(input("Digite o terceiro número: "))
-soma = (num1+num2+num3)
-print("A soma total é:",soma)
+soma = (num1 + num2 + num3)
+print("A soma total é:", soma)
 
-#exercicio 7
+# exercicio 7
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
-soma = (num1+num2)
-print("A soma total entre",num1,"e",num2,"é igual",soma)
+soma = (num1 + num2)
+print("A soma total entre", num1, "e", num2, "é igual", soma)
 
-#exercicio 8
+# exercicio 8
 n1 = float(input("Digite a nota do primeiro bimestre: "))
 n2 = float(input("Digite a nota do segundo bimestre: "))
 n3 = float(input("Digite a nota do terceiro bimestre: "))
 n4 = float(input("Digite a nota do quarto bimestre: "))
-media = (n1+n2+n3+n4)/4
+media = (n1 + n2 + n3 + n4) / 4
 print(media)
 
-#exercicio 9
+# exercicio 9
 metros = float(input("Insira a medida em metros: "))
 cm = metros * 100
-print("Medida em centimetros:",cm,"cm")
+print("Medida em centimetros:", cm, "cm")
 
-#exercicio 10
+# exercicio 10
 num = int(input("Digite um numero: "))
-quadrado = num**2
-cubo = num**3
-print("Número ao quadrado:",quadrado)
-print("Número ao cubo:",cubo)
+quadrado = num ** 2
+cubo = num ** 3
+print("Número ao quadrado:", quadrado)
+print("Número ao cubo:", cubo)
 
-#exercicio 11
+# exercicio 11
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
 div1 = num1 / num2
 div2 = num1 // num2
 
-print("Total decimal da divisão:",div1)
-print("Total inteiro da divisão:",div2)
+print("Total decimal da divisão:", div1)
+print("Total inteiro da divisão:", div2)
 
 # #exercicio 12
 largura = float(input("Digite a largura: "))
 altura = float(input("Digite a altura: "))
 
 metros = largura * altura
-print("Você possui",metros,"metros quadrados")
+print("Você possui", metros, "metros quadrados")
 
-#exercicio 13
+# exercicio 13
 dias = int(input("Insira a quantidade de dias: "))
 horas = int(input("Insira a quantidade de horas: "))
 minutos = int(input("Insira a quantidade de minutos: "))
 segundos = int(input("Insira a quantidade de segundos: "))
 
-conv_m = minutos*60
-conv_h = horas*60
-conv_d = dias*24
-total = (conv_d * (conv_h * conv_m))+segundos
-print("Total em segundos:",total)
+conv_m = minutos * 60
+conv_h = horas * 60
+conv_d = dias * 24
+total = (conv_d * (conv_h * conv_m)) + segundos
+print("Total em segundos:", total)
 
-#exercicio 14
+# exercicio 14
 valor = float(input("Digite o valor do produto: "))
 
-desconto = valor * (10/100)
+desconto = valor * (10 / 100)
 valor_final = valor - desconto
 
-print("Valor total: R$",valor_final)
-print("Desconto: R$",desconto)
+print("Valor total: R$", valor_final)
+print("Desconto: R$", desconto)
---
+++ lista_python2.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+#exercicio 1
+
+#num = int(input("Digite um numero: "))
+#if (num == 0):
+#    print("O numero e zero")
+#elif (num > 0):
+#    print("O numero e positivo")
+#else:
+#    print("O numero e negativo")
+
+#exercicio 2
+#num = int(input("Digite um numero: "))
+#if(num % 2 == 0):
+#    print("O numero e par")
+#else:
+#    print("O numero e impar")
+
+#exercicio 3
+#num1 = int(input("Digite o primeiro numero: "))
+#num2 = int(input("Digite o segundo numero: "))
+#if(num1 > num2):
+#    print(num1, "e maior")
+#else:
+#    print(num2, "e maior")
+
+#exercicio 4
+#idade = int(input("Digite sua idade: "))
+#if(idade >= 18):
+#    print("Voce e maior de idade")
+#else:
+#    print("Voce e menor de idade")
+
+#exercicio 5
+#suaIdade = int(input("Digite sua idade: "))
+#maeIdade = int(input("Digite a idade da sua mae: "))
+#nasc = maeIdade - suaIdade
+#print("Sua mae o teve com ",nasc,"anos de idade")
+
+#exercicio 6
+#print("-"*50)
+
+#exercicio 7
+#num = int(input("Digite um numero: "))
+#if(num % 2 == 0):
+#    print("O numero e par")
+#else:
+#    print("O numero e impar")
+
+#exercicio 8
+#num1 = int(input("Primeiro numero: "))
+#num2 = int(input("Segundo numero: "))
+#if(num1>num2):
+#    print("Imprimindo o maior valor")
+#    print(num1)
+#    print("Imprimindo o menor valor")
+#    print(num2)
+#else:
+#    print("Imprimindo o maior valor")
+#    print(num2#)
+#    print("Imprimindo o menor valor")
+#    print(num1)
+
+#exercicio 9
+#num = 3
+#if(num.__class__ is int):
+#    print("O numero ",num,"e do tipo inteiro")
+#else:
+#    print("O numero ", num, "nao e do tipo inteiro")
+
+#exercicio 10
+#txt = "Olá"
+#if(txt.__class__ is str):
+#    print("O texto",txt,"e do tipo string")
+#else:
+#    print("O texto", txt, "nao e do tipo string#")
+
+#exercicio 11
+#num = 5.4#
+#if(num.__class__ is float):
+#    print("O valor",num,"e do tipo decimal")
+#else:
+#    print("O valor", num, "nao e do tipo decimal")
+
+#exercicio 12
